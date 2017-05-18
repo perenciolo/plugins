47966661db9262d8dcfb63a5bf0d6c6d53af1e34
Gustavo Perenciolo <gustavo@blackowl.com.br>
2017-05-18 17:59:57 -0300

    Create custom post types and custom fields

--- snappy-list-builder/snappy-list-builder.php
+++ snappy-list-builder/snappy-list-builder.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Name: Snappy List Builder
+Plugin Name: Snappy List Builder
 Plugin URI:  http://www.blackowl.com.br
 Description: The ultimate email list building plugin for wordpress. Capture new subscribers. Reward subscribers 
 with a custom download upon opt-in. Build unlimited lists. Import and export subscribers easily with .csv
@@ -11,59 +11,30 @@ License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: br.com.blackowl.snappy-list-builder
 */
-
-
-
-
 /* !0. TABLE OF CONTENTS */
-
-
-
-
 /*
 1. HOOKS
-
 2. SHORTCODES
     2.1 - slb_register_shortcodes()
     2.2 - slb_form_shortcode()
-
 3. FILTERS
-
 4. EXTERNAL SCRIPTS
-
 5. ACTIONS
-
 6. HELPERS
-
 7. CUSTOM POST TYPES
-
 8. ADMIN PAGES
-
 9. SETTINGS
 */
-
-
-
-
-
-
 /* !1. HOOKS */
-
 // 1.1
 // hint: registers all our custom shortcodes on init
 add_action('init', 'slb_register_shortcodes');
-
-
-
-
 /* !2. SHORTCODES */
-
 // 2.1 
 // hint: registers all our custom shortcodes
 function slb_register_shortcodes(){
     add_shortcode('slb_form', 'slb_form_shortcode');
 }
-
 // 2.2 
 function slb_form_shortcode($args, $content='')
 {
@@ -98,59 +69,10 @@ function slb_form_shortcode($args, $content='')
 	// return our results/html
     return $output;
 }
-
-
-
-
-
-
-
 /* !3. FILTERS */
-
-
-
-
-
-
-
 /* !4. EXTERNAL SCRIPTS */
-
-
-
-
-
-
-
 /* !5. ACTIONS */
-
-
-
-
-
-
-
 /* !6. HELPERS */
-
-
-
-
-
-
-
 /* !7. CUSTOM POST TYPES */
-
-
-
-
-
-
-
 /* !8. ADMIN PAGES */
-
-
-
-
-
-
-
 /* !9. SETTINGS */
